c527074938986b57c045ea39d68a1c4c9053794f
JohannesWeinbrecht <johannes.weinbrecht@gmail.com>
2026-05-29 10:14:58 +0200

    added nabla plugin

---
+++ lua/custom/plugins/nabla.lua
@@ -0,0 +1,31 @@
+return {
+  'jbyuki/nabla.nvim',
+  dependencies = {
+    'nvim-neo-tree/neo-tree.nvim',
+    'williamboman/mason.nvim',
+  },
+  --lazy = true,
+
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = { 'latex' },
+      auto_install = true,
+      sync_install = false,
+    }
+  end,
+
+  keys = function()
+    return {
+      {
+        '<leader>np',
+        ':lua require("nabla").popup()<cr>',
+        desc = 'NablaPopUp',
+      },
+      {
+        '<leader>nt',
+        ':lua require("nabla").toggle_virt()<cr>',
+        desc = 'ToggleNablaPreview',
+      },
+    }
+  end,
+}
